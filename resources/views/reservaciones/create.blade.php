ca57efbe28ba4b43f5b88acfae4b5d266a2fed7b
Jorge Gomez <103078336+JorgeIGCZ@users.noreply.github.com>
2022-07-09 13:25:18 -0500

    Misspelling

--- resources/views/reservaciones/create.blade.php
+++ resources/views/reservaciones/create.blade.php
@@ -271,7 +271,7 @@
                 'agente'       : reservacion.elements['agente'].value,
                 'comisionista' : reservacion.elements['comisionista'].value,
                 'cerrador'     : reservacion.elements['cerrador'].value,
-                'total'        : reservacion.elements['subtotal'].getAttribute('value')
+                'total'        : reservacion.elements['subtotal'].getAttribute('value'),
                 'pagos'        : estatus === 'pagar-reservar' ? pagos : {},
                 
                 
