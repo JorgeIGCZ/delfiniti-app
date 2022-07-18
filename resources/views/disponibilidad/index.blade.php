137f2adcca8e5210ee9cd10e403c3f9c1d04b8bf
Jorge Gomez <103078336+JorgeIGCZ@users.noreply.github.com>
2022-07-18 00:22:05 -0500

    refresh  disponibilidad after inactivity

--- resources/views/disponibilidad/index.blade.php
+++ resources/views/disponibilidad/index.blade.php
@@ -1,76 +1,8 @@
 @extends('layouts.app')
 @section('scripts')
     <script>
-        /*
-    		let comisionistasTable;
-    		function verificacionDestroy(id){
-    			Swal.fire({
-    				title: '¿Desea eliminar comisionista?',
-    				text: "Este proceso no se puede revertir!",
-    				icon: 'warning',
-    				showCancelButton: true,
-    				confirmButtonColor: '#3085d6',
-    				cancelButtonColor: '#d33',
-    				confirmButtonText: '¡Si, Eliminar!'
-    			}).then((result) => {
-    				if (result.isConfirmed) {
-    					destroyComisionista(id);
-    				}else{
-    					return false;
-    				}
-    			}) 
-    		}
-    		function destroyComisionista(id){
-    			axios.get(`/comisionistas/destroy/${id}`)
-    			.then(function (response) {
-    				Swal.fire({
-    					icon: 'success',
-    					title: 'Registro eliminado',
-    					showConfirmButton: false,
-    					timer: 1500
-    				})
-    			})
-    			.catch(function (error) {
-    				Swal.fire({
-    					icon: 'error',
-    					title: 'Eliminacion fallida',
-    					showConfirmButton: false,
-    					timer: 1500
-    				})
-    			});
-    			comisionistasTable.ajax.reload();
-    		}
-    		*/
-        /*
-    		function createDisponibilidad(disponibilidad){
-    			axios.post('/disponibilidad/store', {
-    				'_token'  : '{{ csrf_token() }}',
-    				"nombre"  : comisionistas.elements['nombre'].value,
-    				"capacidad": comisionistas.elements['capacidad'].value,
-    				"horarioInicial" : comisionistas.elements['horario-inicial'].value,
-    				"horarioFinal"   : comisionistas.elements['horario-final'].value
-    			})
-    			.then(function (response) {
-    				Swal.fire({
-    					icon: 'success',
-    					title: 'Registro creado',
-    					showConfirmButton: false,
-    					timer: 1500
-    				})
-    			})
-    			.catch(function (error) {
-    				Swal.fire({
-    					icon: 'error',
-    					title: 'Registro fallido',
-    					showConfirmButton: false,
-    					timer: 1500
-    				})
-    			});
-    			comisionistas.reset();
-    			comisionistasTable.ajax.reload();
-    		}
-    		*/
         $(function() {
+            reloadInactividad();
             let reservaciones = new DataTable('.reservaciones-table', {
                 searching: false,
                 paging: false,
@@ -85,44 +17,6 @@
                     }
                 ]
             });
-            /*
-            comisionistasTable = new DataTable('#comisionistas', {
-            	ajax: function (d,cb,settings) {
-            		axios.get('/comisionistas/show')
-            		.then(function (response) {
-            			cb(response.data)
-            		})
-            		.catch(function (error) {
-            		});
-            	},
-            	columns: [
-            		{ data: 'codigo' },
-            		{ data: 'nombre' },
-            		{ data: 'comision' },
-            		{ data: 'iva' },
-            		{ defaultContent: 'Acciones', className: 'dt-center', 'render': function ( data, type, row ) 
-            			{
-            				let removeRow = '';
-            				//if('{{ @session()->get('user_roles')['Alumnos']->Estatus }}' == 'Y'){
-            					removeRow = `| <a href="#" onclick="verificacionDestroy(${row.id})" >Eliminar</a>`;
-            				//}
-            				let view    =   `<small> 
-        									<a href="comisionistas/edit/${row.id}">Editar</a>
-        									${removeRow}
-        								</small>`;
-            				return  view;
-            			}
-            		}
-            	]
-            } );
-            */
-            /*
-            document.getElementById('actividad-form').addEventListener('submit', (event) =>{
-            	event.preventDefault();
-            	const actividad = document.getElementById('actividad-form');
-            	createActividad(actividad);
-            });
-            **/
             document.getElementById('toogle-info').addEventListener('click', (event) => {
                 event.preventDefault();
                 const reservacionesTable = document.querySelectorAll('.reservaciones-table');
@@ -145,6 +39,24 @@
             });
 
         });
+        function reloadInactividad(){
+            let time = new Date().getTime();
+            const setActivityTime = (e) => {
+            time = new Date().getTime();
+            }
+            document.body.addEventListener("mousemove", setActivityTime);
+            document.body.addEventListener("keypress", setActivityTime);
+
+            const refresh = () => {
+            if (new Date().getTime() - time >= 5000) {
+                window.location.reload(true);
+            } else {
+                setTimeout(refresh, 5000);
+            }
+            }
+
+            setTimeout(refresh, 5000);
+        }
     </script>
 @endsection
 @section('content')
