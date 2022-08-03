f7bc891f9208c7a6b741e5d1c049180ac8130802
Jorge Gomez <103078336+JorgeIGCZ@users.noreply.github.com>
2022-08-03 03:40:15 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PaisSeeder::class);
         $this->call(EstadoSeeder::class);
         $this->call(TipoCambioSeeder::class);
-        $this->call(TipoPagoSeeders::class);
+        $this->call(TipoPagoSeeder::class);
     }
 }
