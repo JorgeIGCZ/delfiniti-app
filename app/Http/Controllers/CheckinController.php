846aa1ffbbfa300ffb4bb7a1f91f0fc4a43b3eda
Jorge Gomez <103078336+JorgeIGCZ@users.noreply.github.com>
2022-08-19 00:58:36 -0500

    Update CheckinController.php

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -105,7 +105,7 @@ class CheckinController extends Controller
         $estatusPago    = $reservaciones->getEstatusPagoReservacion($reservacion['id']);
         $fechaActividad = $reservacion['fecha'];
         $today          = date("Y-m-d");
-        if($estatusPago == 2 && $fechaActividad == $today){
+        if($estatusPago == 2 && $fechaActividad <= $today){
             $reservacion           = Reservacion::find($reservacion['id']);
             $reservacion->check_in = 1;
             $reservacion->save();
