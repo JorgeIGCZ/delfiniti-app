bbbb0fc2d7af6fa703ec40c5574433f6b1c8f324
Jorge Gomez <103078336+JorgeIGCZ@users.noreply.github.com>
2022-10-08 11:53:28 -0500

    Update edit.blade.php

--- resources/views/reservaciones/edit.blade.php
+++ resources/views/reservaciones/edit.blade.php
@@ -502,8 +502,8 @@
                                     </div>
                                     
                                     @can('Reservaciones.update') 
+                                    {{$reservacion->estatusPago}}
                                         @if($reservacion->estatusPago !== 2)
-                                        {{$reservacion->estatusPago}}
                                             <div class="form-group col-2 mt-0 mb-0">
                                                 <button class="btn btn-info btn-block mt-33" id="actualizar" tabindex="21">Actualizar</button>
                                             </div>
