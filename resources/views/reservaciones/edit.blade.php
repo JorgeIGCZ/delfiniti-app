bd1cafadb73ac4d29654066a4465a849bc97ed48
Jorge Gomez <103078336+JorgeIGCZ@users.noreply.github.com>
2022-10-10 15:27:52 -0500

    Update edit.blade.php

--- resources/views/reservaciones/edit.blade.php
+++ resources/views/reservaciones/edit.blade.php
@@ -80,7 +80,7 @@
                 ('{{$pago->tipo_pago_id}}' == '2' ? `${formatter.format('{{$pago->cantidad}}')} USD * ${'{{$pago->tipo_cambio_usd}}'}` : formatter.format('{{$pago->cantidad}}')),
                 '{{@$pago->tipoPago->nombre}} {{@$pago->descuentoCodigo->nombre}}',
                 '{{$pago->created_at}}',
-                ((accion === 'pago' && ![1,2,3,8].includes({{$pago->tipo_pago_id}})) 
+                ((accion === 'edit' && ![1,2,3,8].includes({{$pago->tipo_pago_id}}) || {{Auth::user()->hasRole('Administrador')}}) 
                     ? `<a href="#!" class='eliminar-celda' class='eliminar'>Eliminar</a>` 
                     : '')
             ]];
